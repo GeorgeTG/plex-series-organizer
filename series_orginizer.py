1d5facaffb3fd7f7ded68e8cdde214190cf5471d
GeorgeTG <george_gougoudis@hotmail.com>
2015-11-09 21:21:00 +0200

    added disclaimer

--- series_orginizer.py
+++ series_orginizer.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
-VERSION = 0.5
+
+LICENSE = """
+    PlexSeriesOrganizer - A tool to organize your plex series, without moving files.
+    Copyright (C) 2015 George T. Gougoudis <george_gougoudis@hotmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+VERSION = 0.7
+
 import argparse
 import re
 from os import listdir, symlink, makedirs, remove
@@ -27,8 +47,8 @@ class Colors(object):
     RST = '\033[0m'
 
     @staticmethod
-    def print_sep(length):
-        print('-' * (length + MARGIN))
+    def print_sep(color='GRN', length = 64):
+        Colors.print(color, '-' * (length + MARGIN))
 
     @staticmethod
     def print_header(text):
@@ -59,6 +79,8 @@ class Colors(object):
     @staticmethod
     def print_wrn(warn):
         print("{0}{1}{2}{3}".format(Colors.WRN, '[WARN] ', warn, Colors.RST))
+
+
 class Bundle:
     def __init__(self, **entries):
         self.__dict__.update(entries)
@@ -79,7 +101,6 @@ def check_dir(path):
         Colors.print_err('Not a directory: {0}'.format(path))
         return False
     return True
-
 
 def parse_args():
     parser = argparse.ArgumentParser(
